af3ff8cffc7b81ed7eedfe9da98b64aa7c6e608c
Drew Lytle <aml3ed@virginia.edu>
2022-04-09 08:34:28 -0400

    Init

---
+++ app/root.tsx
@@ -0,0 +1,71 @@
+import {
+  json,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useCatch,
+} from "remix";
+import type { LinksFunction, MetaFunction, LoaderFunction } from "remix";
+
+import tailwindStylesheetUrl from "./styles/tailwind.css";
+import { getUser } from "./auth.server";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+};
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Remix Notes",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+type LoaderData = {
+  user: Awaited<ReturnType<typeof getUser>>;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return json<LoaderData>({
+    user: await getUser(request),
+  });
+};
+
+export default function App() {
+  return (
+    <html lang="en" className="h-full">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-full">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  return (
+    <html>
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>Uh oh!</h1>
+        <h2>
+          {caught.status} {caught.statusText}
+        </h2>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
