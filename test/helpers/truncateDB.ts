58c0be491e67213f34580f3cda63827c8c9944f1
Drew Lytle <aml3ed@virginia.edu>
2022-05-05 16:44:48 -0400

    Fix truncate DB command

--- test/helpers/truncateDB.ts
+++ test/helpers/truncateDB.ts
@@ -2,15 +2,16 @@ import { db } from "~/db.server";
 
 export async function truncateDB() {
   const tablenames = await db.$queryRaw<
-    Array<{ tablename: string }>
-  >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
+    Array<{ TABLE_NAME: string }>
+  >`SELECT table_name FROM information_schema.tables WHERE table_schema = 'test'`;
 
-  for (const { tablename } of tablenames) {
-    if (tablename !== "_prisma_migrations") {
+  for (const { TABLE_NAME: tablename } of tablenames) {
+    // console.log(tablename);
+    if (tablename && tablename !== "_prisma_migrations") {
       try {
-        await db.$executeRawUnsafe(
-          `TRUNCATE TABLE "public"."${tablename}" CASCADE;`
-        );
+        await db.$executeRawUnsafe(`SET FOREIGN_KEY_CHECKS = 0;`);
+        await db.$executeRawUnsafe(`TRUNCATE TABLE ${tablename}`);
+        await db.$executeRawUnsafe(`SET FOREIGN_KEY_CHECKS = 1;`);
       } catch (error) {
         console.log({ error });
       }
