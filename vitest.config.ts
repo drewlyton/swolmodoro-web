1a1260378c72e66f2548cabf6bb5e19bfca8f350
Drew Lytle <aml3ed@virginia.edu>
2022-05-05 18:54:23 -0400

    Specify tsconfig so no crawl

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,12 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+  ],
   test: {
     globals: true,
     environment: "happy-dom",
